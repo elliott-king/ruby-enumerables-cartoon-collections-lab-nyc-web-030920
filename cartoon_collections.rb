a9bd54a6a292f29da5fabb3ffac990966683379e
Elliott King <8884571+elliott-king@users.noreply.github.com>
2020-02-29 22:03:26 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,21 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves dwarves
+  dwarves.each_with_index {|v,i| puts("#{(i + 1).to_s}. #{v}")}
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet planeteers
+  planeteers.map { |e| "#{e.capitalize}!" }
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls arr
+  arr.any? { |e| e.size > 4 }
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese cheeses
   cheese_types = ["cheddar", "gouda", "camembert"]
+  cheese_types.each do |type|
+    if cheeses.include?(type)
+      return type
+    end
+  end
+  return nil
 end
